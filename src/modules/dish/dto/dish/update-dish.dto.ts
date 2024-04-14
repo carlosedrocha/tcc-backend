c86dea0d39d4566929414e42de509003704be86c
Kadu <64608697+carlosedrocha@users.noreply.github.com>
2024-04-14 15:18:44 -0300

    feat: dish crud

---
+++ src/modules/dish/dto/dish/update-dish.dto.ts
@@ -0,0 +1,23 @@
+import { IsArray, IsNumber, IsOptional, IsString } from 'class-validator';
+
+export class UpdateDishDto {
+  @IsOptional()
+  @IsString()
+  name?: string;
+
+  @IsOptional()
+  @IsNumber()
+  price?: number;
+
+  @IsOptional()
+  @IsString()
+  description?: string;
+
+  @IsOptional()
+  @IsArray()
+  categoriesIds?: string[];
+
+  @IsOptional()
+  @IsArray()
+  itemsIds?: string[];
+}
