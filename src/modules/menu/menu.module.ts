60904de96efe4f783c5584ab92e7987716094bba
Kadu <64608697+carlosedrocha@users.noreply.github.com>
2024-04-22 14:38:57 -0300

    refactor: included prisma service on menu module

--- src/modules/menu/menu.module.ts
+++ src/modules/menu/menu.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { MenuController } from './menu.controller';
 import { MenuService } from './menu.service';
+import { PrismaService } from '../prisma/prisma.service';
+
 
 @Module({
   controllers: [MenuController],
-  providers: [MenuService]
+  providers: [MenuService, PrismaService]
 })
 export class MenuModule {}
