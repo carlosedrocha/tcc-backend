4be3b193127bfe8cceb3ff5e17caf5329dae4f9a
Rafael Zagui <rafagm02@gmail.com>
2024-09-29 20:13:58 -0300

    bell waiter client

--- src/modules/waiter-bell/waiter-bell.gateway.ts
+++ src/modules/waiter-bell/waiter-bell.gateway.ts
@@ -1,38 +1,45 @@
 import { UseGuards } from '@nestjs/common';
 import {
-    WebSocketGateway,
-    WebSocketServer,
-    SubscribeMessage,
-    MessageBody,
-    ConnectedSocket,
-  } from '@nestjs/websockets';
-  import { Server, Socket } from 'socket.io';
+  WebSocketGateway,
+  WebSocketServer,
+  SubscribeMessage,
+  MessageBody,
+  ConnectedSocket,
+} from '@nestjs/websockets';
+import { Server, Socket } from 'socket.io';
 import { AccessTokenGuard } from 'src/common/guards';
 import { TabService } from '../tab/tab.service';
 import { Tab } from '@prisma/client';
-  
-  @WebSocketGateway({
-    cors: {
-      origin: '*', // Permitir qualquer origem (ajuste conforme necessário)
-    },
-  })
-  @UseGuards(AccessTokenGuard)
-  export class WaiterBellGateway{
-    @WebSocketServer()
-    server: Server;
-  // Injeção de dependência de TabService no construtor
+
+@WebSocketGateway({
+  cors: {
+    origin: '*', // Permitir qualquer origem
+  },
+})
+@UseGuards(AccessTokenGuard)
+export class WaiterBellGateway {
+  @WebSocketServer()
+  server: Server;
+
   constructor(private readonly tabService: TabService) {}
-    // constructor(tabService:TabService){}
-    // Método que será chamado quando o cliente apertar a campainha
-    @SubscribeMessage('callWaiter')
-   async handleCallWaiter(
-      @MessageBody() data: { tabId: string },
-      @ConnectedSocket() client: Socket,
-    ) {
-      const dataString = JSON.stringify(data).replace(/"/g, '');
-       const tab:Tab = await this.tabService.getTabBellById(dataString);
-       console.log(tab)
-       console.log(tab.id+" | "+tab.status)
-      this.server.emit('Cliente:', "");
-    }
+
+  // Método que será chamado quando o cliente apertar a campainha
+  @SubscribeMessage('callWaiter')
+  async handleCallWaiter(
+    @MessageBody() data: { tabId: string },
+    @ConnectedSocket() client: Socket,
+  ) {
+    // Obtenha informações do Tab
+    const tab: Tab = await this.tabService.getTabBellById(data.tabId);
+    
+    // Log para verificar se os dados foram retornados corretamente
+    console.log('Tab Data:', tab);
+
+    // Emitir um evento específico para a interface do garçom
+    this.server.emit('waiterNotification', {
+      tabId: tab.id,
+      status: tab.status,
+      message: `Pedido da mesa ${tab.id}: Status ${tab.status}`,
+    });
   }
+}
